e780c76b7e654d9ea1916d45ea293fc394185c68
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-10-03 12:20:31 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public main {
     public static void main(String[] args) {
-        SevenWonders sw = new SevenWonders();
-        sw.run();
+        SevenWonders sw = new SevenWonders(); //game thing
+        sw.run(); //idk
     }
 }
