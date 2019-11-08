592403c899f75eebe0a1eb350d928fc0d23b06f6
satvah321 <56130370+satvah321@users.noreply.github.com>
2019-11-08 11:42:41 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -8,7 +8,7 @@ public abstract class Card implements Comparable {
 	String color;
 	String freeCard;
 	
-	public Card(String nam,int ag,ArrayList<String> cos,String colr,ArrayList<String> chai,String freeCar)
+	public Card(String nam,int ag,ArrayList<String> cos,String colr,ArrayList<String> chai, String freeCar)
 	{
 		name = nam;
 		age = ag;
@@ -43,17 +43,43 @@ public abstract class Card implements Comparable {
 	}
 	public int compareTo(Card card)
 	{
-		if(card.getColor().compareTo(color) < 0)
-		{
-			return 1;
-		}
-		else if(card.getColor().compareTo(color) > 0)
-		{
+		int num1 = 0;
+		int num2 = 0;
+		if(color.equals("brown"))
+			num1 = 0;
+		else if(color.equals("silver"))
+			num1 = 1;
+		else if(color.equals("green"))
+			num1 = 2;
+		else if(color.equals("blue"))
+			num1 = 3;
+		else if(color.equals("red"))
+			num1 = 4;
+		else if(color.equals("gold"))
+			num1 = 5;
+		else
+			num1 = 6;
+		
+		if(card.getColor().equals("brown"))
+			num2 = 0;
+		else if(card.getColor().equals("silver"))
+			num2 = 1;
+		else if(card.getColor().equals("green"))
+			num2 = 2;
+		else if(card.getColor().equals("blue"))
+			num2 = 3;
+		else if(card.getColor().equals("red"))
+			num2 = 4;
+		else if(card.getColor().equals("gold"))
+			num2 = 5;
+		else
+			num2 = 6;
+		
+		if(num1 < num2)
 			return -1;
-		}
-		else 
-		{
+		else if(num1 > num2)
+			return 1;
+		else
 			return 0;
-		}
 	}
 }
