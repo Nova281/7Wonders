f23f75d26a0f533e61cb4714ec8d0bed889f631a
Benz Le <33076057+Nova281@users.noreply.github.com>
2019-11-09 00:30:11 -0600

    Tint to bkgd

--- GamePanel.java
+++ GamePanel.java
@@ -12,11 +12,14 @@ public class GamePanel extends JPanel {
 	{
 		currentBoard = null;
 		wonderImg = ImageIO.read(getClass().getResource(currentBoard.getName()));
+		tintBkgd = ImageIO.read(getClass().getResource("TintBkgd"));
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		g.drawImage(wonderImg, 0, 0, 1920, 1080, this);
+		g.drawImage(TintBkgd.)
+		
 	}
 	
 	public Wonder updateCurrentBoard(Player currentPlayer)
