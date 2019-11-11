ee90f4e84a4ff4d21b6c266a6f241cd3ffed9ca9
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-11-11 11:39:03 -0600

    Update ResourceCard.java

--- ResourceCard.java
+++ ResourceCard.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class ResourceCard extends Card {
 	
-	ArrayList<String> resources;
+	private ArrayList<String> resources;
+	private boolean isChoice;
 	
 	public ResourceCard(String name, int age, ArrayList<String> cost, String color, ArrayList<String> chain, String freeCard) {
 		super(name, age, cost, color, chain, freeCard);
+		isChoice = false;
 		assignResource();
 	}
 	
@@ -19,12 +21,42 @@ public class ResourceCard extends Card {
 			else
 				resources.add("loom");
 		}
-		else {
-			if(name.equals("Lumber Yard")) {
-				
+		else if(age == 1){
+			if(name.equals("Lumber Yard"))
+				resources.add("wood");
+			else if(name.equals("Stone Pit"))
+				resources.add("stone");
+			else if(name.equals("Clay Pool"))
+				resources.add("clay");
+			else if(name.equals("Ore Vein"))
+				resources.add("ore");
+			else if(name.equals("Clay Pit")) {
+				resources.add("clay");
+				resources.add("ore");
+				isChoice = true;
 			}
-			else {
-				
+			else {	//Timber Yard
+				resources.add("stone");
+				resources.add("wood");
+				isChoice = true;
+			}
+		}
+		else {
+			if(name.equals("Sawmill")) {
+				resources.add("wood");
+				resources.add("wood");
+			}
+			else if(name.equals("Quarry")) {
+				resources.add("stone");
+				resources.add("stone");
+			}
+			else if(name.equals("Brickyard")) {
+				resources.add("stone");
+				resources.add("stone");
+			}
+			else {	//Foundry
+				resources.add("ore");
+				resources.add("ore");
 			}
 		}
 	}
@@ -37,10 +69,8 @@ public class ResourceCard extends Card {
 		return resources;
 	}
 	
-	public boolean choiceResource() {
-		if(name.equals("Timber Yard") || name.equals("Clay Pit"))
-			return true;
-		return false;
+	public boolean isChoice() {
+		return isChoice;
 	}
 
 }
