26a35a025c7e99268c015f4be913a5bcf3d4d31c
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-11-11 11:48:21 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,56 +1,66 @@
 import java.util.ArrayList;
 
-public class ResourceCard extends Card {
+public abstract class Card implements Comparable {
 	
-	ArrayList<String> resources;
+	private String name;
+	private int age;
+	private ArrayList<String> cost;
+	private ArrayList<String> chain;
+	private String color;
+	private String freeCard;
 	
-	public ResourceCard(String name, int age, ArrayList<String> cost, String color, ArrayList<String> chain, String freeCard) {
-		super(name, age, cost, color, chain, freeCard);
-		assignResource();
+	public Card(String name, int age, ArrayList<String> cost, String color, ArrayList<String> chain, String freeCard)
+	{
+		this.name = name;
+		this.age = age;
+		this.cost = cost;
+		this.color = color;
+		this.chain = chain;
+		this.freeCard = freeCard;
 	}
 	
-	private void assignResource() {
-		resources = new ArrayList<>();
-		if(color.equals("silver")) {
-			if(name.equals("Press"))
-				resources.add("papyrus");
-			else if(name.equals("Glassworks"))
-				resources.add("glass");
-			else
-				resources.add("loom");
-		}
-		else {
-			if(name.equals("Lumber Yard"))
-				resources.add("wood");
-			else if(name.equals("Stone Pit"))
-				resources.add("stone");
-			else if(name.equals("Clay Pool"))
-				resources.add("clay");
-			else if(name.equals("Ore Vein"))
-				resources.add("ore");
-			else if(name.equals("Clay Pit")) {
-				resources.add("clay");
-				resources.add("ore");
-			}
-			else {
-				resources.add("stone");
-				resources.add("wood");
-			}
-		}
-	}
+	public String getName() { return name; }
+	public int getAge() { return age; }
+	public ArrayList<String> getCost() 	{ return cost; }
+	public ArrayList<String> getChain() { return chain; }
+	public String getColor() { return color; }
+	public String getFree() { return freeCard; }
 	
-	public int compareTo(Object o) {
-		return super.compareTo(o);
+	public int compareTo(Object o)
+	{
+		Card card = (Card) o;
+		int num1 = 0;
+		int num2 = 0;
+		if(color.equals("brown"))
+			num1 = 0;
+		else if(color.equals("silver"))
+			num1 = 1;
+		else if(color.equals("green"))
+			num1 = 2;
+		else if(color.equals("blue"))
+			num1 = 3;
+		else if(color.equals("red"))
+			num1 = 4;
+		else if(color.equals("gold"))
+			num1 = 5;
+		else
+			num1 = 6;
+		
+		if(card.getColor().equals("brown"))
+			num2 = 0;
+		else if(card.getColor().equals("silver"))
+			num2 = 1;
+		else if(card.getColor().equals("green"))
+			num2 = 2;
+		else if(card.getColor().equals("blue"))
+			num2 = 3;
+		else if(card.getColor().equals("red"))
+			num2 = 4;
+		else if(card.getColor().equals("gold"))
+			num2 = 5;
+		else
+			num2 = 6;
+		
+		return num1-num2;
 	}
-	
-	public ArrayList<String> getResources() {
-		return resources;
-	}
-	
-	public boolean isChoiceResource() {
-		if(name.equals("Timber Yard") || name.equals("Clay Pit"))
-			return true;
-		return false;
-	}
-
 }
