4ed3463130f1ebfdb1a54197cc5bf537ca30d353
satvah321 <56130370+satvah321@users.noreply.github.com>
2019-11-11 11:56:15 -0600

    Update ActionCard.java

--- ActionCard.java
+++ ActionCard.java
@@ -6,14 +6,33 @@ public class ActionCard extends Card{
 	public ActionCard(String name,int age,ArrayList<String> cost,String color,ArrayList<String> chain, String freeCard, String effect)
 	{
 		super(name,age,cost,color,chain,freeCard);
-		setEffect(effect);
-	}
-	private void setEffect(String effect)
-	{
 		this.effect = effect;
 	}
-	public String getEffect()
-	{
-		return effect;
-	}
+//	private void setEffect()
+//	{
+//		if(getAge() == 1)
+//		{
+//			if(getName().equals("East Trading Post"))
+//				effect = "right 1";
+//			else if(getName().equals("West Trading Post"))
+//				effect = "left 1";
+//			else
+//				effect = "both 1";
+//		}
+//		else if(getAge() == 2)
+//		{
+//			if(getName().equals("Vineyard"))
+//				effect = "Brown 1";
+//		}
+//		else
+//		{
+//			if(getColor().contentEquals("purple"))
+//			{
+//				if(getName().equals("Workers Guild"))
+//					effect = "1 Brown";
+//				else if(getName().equals("Craftsman Guild"))
+//					effect = "1 Silver";
+//			}
+//		}
+//	}
 }
