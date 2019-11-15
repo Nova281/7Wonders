f4ae408ca96b7f82c1ae0dad8476250a3d56ff57
Benz Le <33076057+Nova281@users.noreply.github.com>
2019-11-15 12:09:21 -0600

    graphics

--- graphic classes/GamePanel.java
+++ graphic classes/GamePanel.java
@@ -5,25 +5,28 @@ import javax.swing.*;
 import javax.imageio.ImageIO;
 
 public class GamePanel extends JPanel {
+	
 	private Wonder currentBoard;
 	private BufferedImage wonderImg;
 	
 	public GamePanel()
 	{
+	
 		currentBoard = null;
-		wonderImg = ImageIO.read(getClass().getResource(currentBoard.getName()));
-		tintBkgd = ImageIO.read(getClass().getResource("TintBkgd"));
+		wonderImg = ImageIO.read(getClass().getResource(currentBoard.getName())+"tint");
 	}
 	
 	public void paintComponent(Graphics g)
 	{
-		g.drawImage(wonderImg, 0, 0, 1920, 1080, this);
-		g.drawImage(TintBkgd.)
+		super.paintComponent(g);
 		
+		g.drawImage(wonderImg, 0, 0, 1920, 1080, this);
 	}
 	
 	public Wonder updateCurrentBoard(Player currentPlayer)
 	{
 		currentBoard = currentPlayer.getWonder();
 	}
+	
+	
 }
--- graphic classes/MainFrame.java
+++ graphic classes/MainFrame.java
@@ -2,10 +2,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+	private GameState gs;
 	public MainFrame(String title)
 	{
 		super(title);
-		
+		gs = new GameState();
 		
 	}
 }
