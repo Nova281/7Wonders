94628d3ef1123c304c2f7d74d92ea2f839c4c991
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-11-15 12:05:29 -0600

    Add files via upload

---
+++ BoardTester.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+import static java.lang.System.*;
+
+public class BoardTester {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		Board b = new Board();
+		b.printDeck();
+	}
+
+}
