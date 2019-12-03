626ed0357635ca5fa14f4581126e1892eda76a84
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-03 12:11:43 -0600

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,24 +1,40 @@
-import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import static java.lang.System.*;
 
 public class GameState {
 	
 	private int age;
-	private Player[] players;
 	private Board board;
+	private LinkedHashMap<Player, ArrayList<Card>> players;
 	
-	public GameState() throws IOException {
+	public GameState() {
 		board = new Board();
 		age = 1;
-		players = new Player[3];
-		for(int i = 0; i < players.length; i++)
-			players[i] = new Player();
+		players = new LinkedHashMap<>();
+		for(int i = 0; i < 3; i++)
+			players.put(new Player(), new ArrayList<Card>());
 	}
-	
-	public void setWonder(int player, Wonder wonder) {
-		players[player-1].setWonder(wonder);
+	//playerNum can be 1, 2, or 3
+	public void setWonder(int playerNum, Wonder wonder) {
+		Iterator<Player> iter = players.keySet().iterator();
+		for(int i = 0; i < playerNum-1; i++)
+			iter.next();
+		iter.next().setWonder(wonder);
 	}
 	
 	public void run() {
-		board.deal(players[0], players[1], players[2]);
+		board.deal(players);
+		board.printPlayerHand()
 	}
+	
+	public void printPlayers() {
+		Iterator<Player> iter = players.keySet().iterator();
+		for(int i = 0; i < 3; i++)
+			out.println(iter.next());
+	}
+	
+	public Board getBoard() { return board; }
+	public LinkedHashMap<Player, ArrayList<Card>> getPlayerMap() { return players; }
 }
