f4cc671d6a48f61d0b2b923b5d1373401309ad5d
Benz Le <33076057+Nova281@users.noreply.github.com>
2019-12-04 16:46:53 -0600

    Update GamePanel.java

--- graphic classes/GamePanel.java
+++ graphic classes/GamePanel.java
@@ -1,9 +1,15 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
@@ -32,7 +38,7 @@ public class GamePanel extends JPanel {
 		 * wonderImage3 = ImageIO.read(getClass().getResource("Rhodos.jpg"));
 		 */
 		coin1 = ImageIO.read(getClass().getResource("coin1.png"));
-		
+
 		String fName = "PossumSaltareNF.ttf";
 		InputStream is = GamePanel.class.getResourceAsStream(fName);
 		font = Font.createFont(Font.TRUETYPE_FONT, is);
@@ -43,6 +49,7 @@ public class GamePanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
+		// current Player
 		g.drawImage(imgList[(currentPlayer + 2) % 3], 0, 0, 1920, 1080, this);
 
 		g.setColor(new Color(145, 127, 107, 200));
@@ -54,35 +61,37 @@ public class GamePanel extends JPanel {
 		g.setColor(Color.WHITE);
 		g.drawString("Player " + currentPlayer, 900, 778);
 		g.drawImage(currentAgeCard, 0, 780, this);
+		g.drawImage(coin1, 1700, 1000, 40, 40, this);
+		g.drawString(":  " + playerList[(currentPlayer + 2) % 3].getCoins(), 1745, 1025);
 
+		// Player before
 		g.drawImage(imgList[(currentPlayer + 3) % 3], 0, 0, 960, 150, 0, 360, 1920, 720, this);
 		g.drawString("Player " + ((currentPlayer + 3) % 3 + 1), 440, 150);
 		g.drawImage(coin1, 10, 10, 40, 40, this);
-		if (playerList[(currentPlayer + 3) % 3].getWonder().getName().equals("Olympia"))
-		{
+		if (playerList[(currentPlayer + 3) % 3].getWonder().getName().equals("Olympia")) {
 			g.setColor(Color.BLACK);
 			g.drawString(": " + playerList[(currentPlayer + 3) % 3].getCoins(), 55, 35);
 			g.setColor(Color.WHITE);
 		}
-		g.drawString(": " + playerList[(currentPlayer + 3) % 3].getCoins(), 55, 35);
-		
+		g.drawString(":  " + playerList[(currentPlayer + 3) % 3].getCoins(), 55, 35);
+
+		// Player after
 		g.drawImage(imgList[(currentPlayer + 4) % 3], 960, 0, 1920, 150, 0, 360, 1920, 720, this);
 		g.drawString("Player " + ((currentPlayer + 4) % 3 + 1), 1400, 150);
 		g.drawImage(coin1, 970, 10, 40, 40, this);
-		if (playerList[(currentPlayer + 4) % 3].getWonder().getName().equals("Olympia"))
-		{
+		if (playerList[(currentPlayer + 4) % 3].getWonder().getName().equals("Olympia")) {
 			g.setColor(Color.BLACK);
 			g.drawString(": " + playerList[(currentPlayer + 4) % 3].getCoins(), 1015, 35);
 			g.setColor(Color.WHITE);
 		}
-		g.drawString(": " + playerList[(currentPlayer + 4) % 3].getCoins(), 1015, 35);
+		g.drawString(":  " + playerList[(currentPlayer + 4) % 3].getCoins(), 1015, 35);
 	}
 
 	// updated currentWonder onto graphics from Player
 	public void updateCurrentBoard(String playerName) throws IOException {
 		for (int i = 1; i <= imgList.length; i++) {
 			int playerNum = Integer.parseInt(playerName);
-			if (i == playerNum) 
+			if (i == playerNum)
 				currentPlayer = i;
 		}
 	}
@@ -101,7 +110,5 @@ public class GamePanel extends JPanel {
 		currentAgeStr = "age" + i + ".png";
 		currentAgeCard = ImageIO.read(getClass().getResource(currentAgeStr));
 	}
-	
-	
 
 }
