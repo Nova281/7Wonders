2235d76d235c4f2580f918eb40b56f72936adda1
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-04 12:11:36 -0600

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -24,9 +24,11 @@ public class GameState {
 		iter.next().setWonder(wonder);
 	}
 	
-	public void run() {
+	public void deal() {
 		board.deal(players);
-		board.printPlayerHand()
+//		board.printPlayerHand(players, 1);
+//		board.printPlayerHand(players, 2);
+//		board.printPlayerHand(players, 3);
 	}
 	
 	public void printPlayers() {
@@ -36,5 +38,37 @@ public class GameState {
 	}
 	
 	public Board getBoard() { return board; }
+	//playerNum can be 1, 2, 3
+	public Player getplayer(int playerNum) {
+		Iterator<Player> iter = players.keySet().iterator();
+		Player p = new Player();
+		for(int i = 0; i < 3; i++)
+			p = iter.next();
+		return p;
+	}
+	//playerNum can be 1, 2, 3
+	public void discard(int playerNum, Card choice) {
+		Iterator<Player> iter = players.keySet().iterator();
+		Player p = new Player();
+		for(int i = 0; i < 3; i++)
+			p = iter.next();
+		board.discard(p, players.get(p), choice);
+	}
+	
+	public void build(int playerNum, Card choice) {
+		Iterator<Player> iter = players.keySet().iterator();
+		Player p = new Player();
+		for(int i = 0; i < 3; i++)
+			p = iter.next();
+		board.build(p, players.get(p), choice);
+	}
+	
+	public void sacrifice(int playerNum, Card choice) {
+		Iterator<Player> iter = players.keySet().iterator();
+		Player p = new Player();
+		for(int i = 0; i < 3; i++)
+			p = iter.next();
+		board.sacrifice(p, players.get(p), choice);
+	}
 	public LinkedHashMap<Player, ArrayList<Card>> getPlayerMap() { return players; }
 }
