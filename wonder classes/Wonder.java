baf7eb0e5d63d7f501f117db1fd5597ce425d24c
Benz Le <33076057+Nova281@users.noreply.github.com>
2019-12-04 16:34:05 -0600

    Update Wonder.java

--- wonder classes/Wonder.java
+++ wonder classes/Wonder.java
@@ -1,34 +1,36 @@
-import java.util.*;
-public class Wonder
-{
+import java.util.TreeMap;
+
+public class Wonder {
 	private TreeMap<Integer, Boolean> Phases;
 	private String name, resource, effect;
-	
-	public Wonder()
-	{
-		Phases.put(1,false);
-		Phases.put(2,false);
-		Phases.put(3,false);
+
+	public Wonder() {
+		TreeMap<Integer, Boolean> Phases = new TreeMap<>();
+		Phases.put(1, false);
+		Phases.put(2, false);
+		Phases.put(3, false);
 	}
-	public String getName()
-	{
+
+	public String getName() {
 		return name;
 	}
-	
-	public boolean getPhaseState(int stage) { return Phases.get(stage); }
-	
-	public int getPhase1()
-	{
+
+	public boolean getPhaseState(int stage) {
+		return Phases.get(stage);
+	}
+
+	public int getPhase1() {
 		return 3;
 	}
-	
-	public int getPhase3()
-	{
+
+	public int getPhase3() {
 		return 7;
 	}
+
 	public String getResource() {
 		return resource;
 	}
+
 	public String getEffect() {
 		return effect;
 	}
