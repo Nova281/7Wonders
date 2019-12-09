362d84e7e4b7712f6a2e6d650724bbf2d07771c9
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-08 23:33:39 -0600

    Add files via upload

---
+++ src/GameState.java
@@ -0,0 +1,81 @@
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import static java.lang.System.*;
+
+import java.io.IOException;
+
+public class GameState {
+	
+	private int age;
+	private Board board;
+	private LinkedHashMap<Player, ArrayList<Card>> players;
+	
+	public GameState() {
+		board = new Board();
+		age = 1;
+		players = new LinkedHashMap<>();
+		for(int i = 1; i < 4; i++)
+			players.put(new Player(i), new ArrayList<Card>());
+	}
+	//playerNum can be 1, 2, or 3
+	public void setWonder(int playerNum, Wonder wonder) {
+		Player p = getPlayer(playerNum);
+		p.setWonder(wonder);
+	}
+	
+	public void deal() {
+		board.deal(players);
+//		board.printPlayerHand(players, 1);
+//		board.printPlayerHand(players, 2);
+//		board.printPlayerHand(players, 3);
+	}
+	
+	public void printPlayerHand(int playerNum) {
+		for(Player k: players.keySet())
+			out.println(players.get(k));
+		//out.println(players.get(getPlayer(playerNum)));
+	}
+	
+	public Board getBoard() { return board; }
+	//playerNum can be 1, 2, 3
+	public Player getPlayer(int playerNum) {
+		Iterator<Player> iter = players.keySet().iterator();
+		Player p = new Player(0);
+		for(int i = 0; i < 3; i++)
+			p = iter.next();
+		return p;
+	}
+	//playerNum can be 1, 2, 3
+	public void discard(int playerNum, Card choice) {
+		Player p = getPlayer(playerNum);
+		board.discard(p, players.get(p), choice);
+	}
+	
+	public void build(int playerNum, Card choice) {
+		Player p = getPlayer(playerNum);
+		board.build(p, players.get(p), choice);
+	}
+	
+	public void sacrifice(int playerNum, Card choice) {
+		Player p = getPlayer(playerNum);
+		board.sacrifice(p, players.get(p), choice);
+	}
+	public void passCards() {
+		players = board.passCards(age, players);
+	}
+	public int nextAge() {
+		for(int i = 1; i < 4; i++)
+			players.put(getPlayer(i), new ArrayList<>());
+		try {
+			board.createDeck(++age);
+		} catch (IOException e) { }
+		board.deal(players);
+		return age;
+	}
+	public int getAge() { return age; }
+	public LinkedHashMap<Player, ArrayList<Card>> getPlayerMap() { return players; }
+//	public boolean buildWonder(int playerNum) {
+//		return getPlayer(playerNum).buildWonder();
+//	}
+}
