c6dab1aa78398ea35e4edcac4f14c165606fbf88
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-10 01:03:51 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,8 @@ import static java.lang.System.out;
 public class Main {
 
 	public static void main(String[] args) {
-		//graphics
-		GameState gs = new GameState(); //will be in graphics, just do graphics.getGameState();
-		gs.deal();
-		for(int i = 1; i < 4; i++) {
-			out.println("Player " + gs.getPlayer(i).getPlayerNum() + ": ");
-			gs.printPlayerHand(i);
-			out.println("Wonder: ");
-			out.println(gs.getPlayer(i).getWonder());
-		}
-		
-		
+		Board b = new Board();
+		b.run();
 	}
 
 }
