298f28e36531c51f5e072baff7ddf33df03f834d
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-09 20:49:59 -0600

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 import java.util.LinkedHashMap;
@@ -44,26 +46,39 @@ public class Player {
 		sciences = new TreeMap<String,Integer>();
 	}
 	//sets war points, takes in age and if won
-	public void setWar(int age, boolean win)
+	public void wageWar(int age, Player p)
 	{
-		if(win == false) {
+		if(p.getMP() > mp) {
 			loss++;
 			warPoints-=1;
+			p.setWar(age, true);
+			return;
 		}
+		else if(p.getMP() == mp)
+			return;
 		else if(age == 1)
-		{
 			warPoints += 1;
-			wins++;
-		}
 		else if(age == 2)
-		{
 			warPoints += 3;
-			wins++;
-		}
 		else
-		{
 			warPoints += 5;
+		wins++;
+		p.setWar(age, false);
+	}
+	
+	private void setWar(int age, boolean win) {
+		if(win) {
 			wins++;
+			if(age == 1)
+				warPoints++;
+			else if(age == 2)
+				warPoints+=3;
+			else
+				warPoints+=5;
+		}
+		else {
+			loss++;
+			warPoints--;
 		}
 	}
 	
@@ -221,10 +236,9 @@ public class Player {
 				for(ArrayList<String> k: combinations) {
 					for(int i = cost.size()-1; i >= 0; i--)
 						if(k.contains(cost.get(i))) {
-							cost.remove(i);
 							k.remove(i);
 						}
-					if(cost.size() == 0)
+					if(k.size() == 0)
 						return true;
 				}
 			}
@@ -373,7 +387,7 @@ public class Player {
 		}
 		else if(wonder.getName().equals("Halicarnassus"))
 		{
-			Halicarnassus alex = (Halicarnassus)wonder;
+			Halikarnassos alex = (Halikarnassos)wonder;
 			String[] array = alex.getResWonder(stage).split(" ");
 			ArrayList<String> res = new ArrayList<String>();
 			for(int i = 0; i < array.length; i++)
@@ -460,123 +474,180 @@ public class Player {
 	public String toString() { return cards.toString(); }
 	public int getWP() { return warPoints; }
 	public Wonder getWonder() { return wonder; }
-	public boolean trade(Player p1, Player p2, ArrayList<String> cost)
+	public boolean trade(Player left, Player right, ArrayList<String> cost)
 	{
-		int p1Removed = 0, p2Removed = 0;
-		//check through normal resources of p1
-		if(cost.size() == 1 && cost.get(0).equals(" "))
-			return true;
-		TreeMap<String, Integer> res = new TreeMap<String, Integer>();
-		for(String k: p1.getResources().keySet())
-			res.put(k, p1.getResources().get(k));
-		for(int i = cost.size()-1; i >= 0; i--) {
-			String resource = cost.get(i);
-			if(res.containsKey(resource)) {
-				int numRes = res.get(cost.get(i))-1;
-				cost.remove(i);
-				p1Removed++;
-				if(numRes == 0)
-					res.remove(resource);
-				else
-					res.put(resource, numRes);
-			}
-		}
+		LinkedHashMap<String, Integer> rColors = new LinkedHashMap<>();
+		rColors.put("brown", 0);
+		rColors.put("silver", 0);
+		String[] brown = {"wood", "stone", "ore", "clay"};
+		ArrayList<String> brownList = (ArrayList<String>) Arrays.asList(brown);
+		String[] silver = {"cloth", "papyrus", "glass"};
+		ArrayList<String> silverList = (ArrayList<String>) Arrays.asList(silver);
+		ActionCard ETP = (new ActionCard("East Trading Post;gold;1; ;Forum; "));
+		ActionCard WTP = (new ActionCard("West Trading Post;gold;1; ;Forum; "));
+		ActionCard Market = (new ActionCard("Marketplace;gold;1; ;Caravansery; "));
+		ResourceCard[] brownCard = {new ResourceCard("Clay Pit;brown;1; ; ; "), //0
+				new ResourceCard("Clay Pool;brown;1; ; ; "), //1
+				new ResourceCard("Lumber Yard;brown;1; ; ; "), //2
+				new ResourceCard("Ore Vein;brown;1; ; ; "), //3
+				new ResourceCard("Stone Pit;brown;1; ; ; "), //4
+				new ResourceCard("Timber Yard;brown;1; ; ; ")}; //5
+		ResourceCard[] silverCard = {new ResourceCard("Glassworks;silver;1; ; ;"), //0
+				new ResourceCard("Loom;silver;1; ; ; "), //1
+				new ResourceCard("Press;silver;1; ; ; ")}; //2
 		
-		//check through choice resources of p2
-		int costSize = cost.size();
-		if(cost.size() > 0 && cost.size() <= p1.getChoiceRes().size()) {
-			ArrayList<String> c = new ArrayList<String>();
-			ArrayList<ArrayList<String>> combinations = new ArrayList<>();
-			createCombo(c, 0, "");
-			String[] a;
-			for(String k: c) {
-				a = k.split(";");
-				ArrayList<String> b = new ArrayList<>();
-				for(int i = 1; i < a.length; i++)
-					b.add(a[i]);
-				combinations.add(b);
-			}
-			if(combinations.contains(cost))
-				return true;
-			else {
-				ArrayList<String> small = new ArrayList<>(cost);
-				for(ArrayList<String> k: combinations) {
-					for(int i = cost.size()-1; i >= 0; i--)
-						if(k.contains(cost.get(i))) {
-							cost.remove(i);
-							k.remove(i);
-						}
-					if(cost.size() == 0) {
-						p1Removed += costSize;
-						return true;
-					}
-					else if(small.size() > cost.size()) {
-						small = cost;
-					}
-				}
-				cost = small;
-			}
-		}
-		else {
-			p1.addCoins(p1Removed);
-			return true;
-		}
-		//check through normal resources of p2
-		res = new TreeMap<String, Integer>();
-		for(String k: p2.getResources().keySet())
-			res.put(k, p2.getResources().get(k));
 		for(int i = cost.size()-1; i >= 0; i--) {
-			String resource = cost.get(i);
-			if(res.containsKey(resource)) {
-				int numRes = res.get(cost.get(i))-1;
-				cost.remove(i);
-				p2Removed++;
-				if(numRes == 0)
-					res.remove(resource);
-				else
-					res.put(resource, numRes);
+			String r = cost.get(i);
+			if(left.hasResource(r)) {
+				cost.remove(r);
+				if(brownList.contains(r))
+					
+			}
+			else if(right.hasResource(r)) {
+				cost.remove(r);
 			}
 		}
+		Iterator<String> iter = left.getResources().keySet().iterator();
 		
-		//check through choice resources of p2
-		costSize = cost.size();
-		if(cost.size() > 0 && cost.size() <= p2.getChoiceRes().size()) {
-			ArrayList<String> c = new ArrayList<String>();
-			ArrayList<ArrayList<String>> combinations = new ArrayList<>();
-			createCombo(c, 0, "");
-			String[] a;
-			for(String k: c) {
-				a = k.split(";");
-				ArrayList<String> b = new ArrayList<>();
-				for(int i = 1; i < a.length; i++)
-					b.add(a[i]);
-				combinations.add(b);
-			}
-			if(combinations.contains(cost))
-				return true;
-			else {
-				for(ArrayList<String> k: combinations) {
-					for(int i = cost.size()-1; i >= 0; i--)
-						if(k.contains(cost.get(i))) {
-							cost.remove(i);
-							k.remove(i);
-						}
-					if(cost.size() == 0) {
-						p1Removed += costSize;
-						return true;
-					}
-				}
-			}
-			return false;
-		}
-		else {
-			p2.addCoins(p2Removed);
-			return true;
-		}
+		//check through normal resources of left
+//		if(cost.size() == 1 && cost.get(0).equals(" "))
+//			return true;
+//		TreeMap<String, Integer> res = new TreeMap<String, Integer>();
+//		for(String k: left.getResources().keySet())
+//			res.put(k, left.getResources().get(k));
+//		for(int i = cost.size()-1; i >= 0; i--) {
+//			String resource = cost.get(i);
+//			if(res.containsKey(resource)) {
+//				int numRes = res.get(cost.get(i))-1;
+//				if(brownList.contains(cost.remove(i)))
+//					removedColors.put("brown", removedColors.get("brown") + 1);
+//				else
+//					removedColors.put("silver", removedColors.get("silver") + 1);
+//				if(numRes == 0)
+//					res.remove(resource);
+//				else
+//					res.put(resource, numRes);
+//			}
+//		}
+//		if(cost.size() == 0) {
+//			//brown cards
+//			if(left.hasCard(WTP)) {
+//				left.addCoins(removedColors.get("brown"));
+//				coins-=removedColors.get("brown");
+//			}
+//			else {
+//				left.addCoins(leftR*2);
+//				coins-=(leftR*2);
+//			}
+//			return true;
+//		}
+//		//check through choice resources of left
+//		int costSize = cost.size();
+//		ArrayList<String> c = new ArrayList<String>();
+//		ArrayList<ArrayList<String>> combinations = new ArrayList<>();
+//		createCombo(c, 0, "");
+//		String[] a;
+//		for(String k: c) {
+//			a = k.split(";");
+//			ArrayList<String> b = new ArrayList<>();
+//			for(int i = 1; i < a.length; i++)
+//				b.add(a[i]);
+//			combinations.add(b);
+//		}
+//		ArrayList<String> small = new ArrayList<>(cost);
+//		for(ArrayList<String> k: combinations) {
+//			for(int i = cost.size()-1; i >= 0; i--)
+//				if(k.contains(cost.get(i))) {
+//					k.remove(i);
+//				}
+//			if(k.size() == 0) {
+//				leftR += costSize;
+//				if(left.hasCard(WTP)) {
+//					left.addCoins(leftR);
+//					coins-=leftR;
+//				}
+//				else {
+//					left.addCoins(leftR*2);
+//					coins-=(leftR*2);
+//				}
+//				return true;
+//			}
+//			else if(small.size() > cost.size()) {
+//				small = cost;
+//			}
+//		}
+//		cost = small;
+//		leftR+=(costSize-cost.size());
+//		
+//		//check through normal resources of right
+//		res = new TreeMap<String, Integer>();
+//		for(String k: right.getResources().keySet())
+//			res.put(k, right.getResources().get(k));
+//		for(int i = cost.size()-1; i >= 0; i--) {
+//			String resource = cost.get(i);
+//			if(res.containsKey(resource)) {
+//				int numRes = res.get(cost.get(i))-1;
+//				cost.remove(i);
+//				rightR++;
+//				if(numRes == 0)
+//					res.remove(resource);
+//				else
+//					res.put(resource, numRes);
+//			}
+//		}
+//		if(cost.size() == 0) {
+//			if(right.hasCard(ETP)) {
+//				right.addCoins(rightR);
+//				coins-=rightR;
+//			}
+//			else {
+//				right.addCoins(rightR*2);
+//				coins-=(rightR*2);
+//			}
+//			return true;
+//		}
+//		//check through choice resources of right
+//		costSize = cost.size();
+//		if(cost.size() > 0 && cost.size() <= right.getChoiceRes().size()) {
+//			c = new ArrayList<String>();
+//			combinations = new ArrayList<>();
+//			createCombo(c, 0, "");
+//			for(String k: c) {
+//				a = k.split(";");
+//				ArrayList<String> b = new ArrayList<>();
+//				for(int i = 1; i < a.length; i++)
+//					b.add(a[i]);
+//				combinations.add(b);
+//			}
+//			if(combinations.contains(cost))
+//				return true;
+//			else {
+//				for(ArrayList<String> k: combinations) {
+//					for(int i = cost.size()-1; i >= 0; i--)
+//						if(k.contains(cost.get(i))) {
+//							k.remove(i);
+//						}
+//					if(k.size() == 0) {
+//						leftR += costSize;
+//						return true;
+//					}
+//				}
+//			}
+//			return false;
+//		}
+//		else {
+//			right.addCoins(rightR);
+//			return true;
+//		}
 	}
 	public int getVP()
 	{
 		return vp;
+	}
+	public boolean hasResource(String res) {
+		if(resources.containsKey(res))
+			return true;
+		return false;
 	}
 	public LinkedHashMap<String, ArrayList<Card>> getCards() { return cards; }
 	
