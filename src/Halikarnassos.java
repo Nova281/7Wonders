82adc22e21941a10445c3600ec2bdf18e25ce33c
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-11 00:29:29 -0600

    Update Halikarnassos.java

--- src/Halikarnassos.java
+++ src/Halikarnassos.java
@@ -1,19 +1,19 @@
 
-public class Halikarnassos extends Wonder {
-	public Halikarnassos() {
-		super("Halicarnassus", "cloth");
+public class Halikarnassos extends Wonder 	
+{
+	public Halikarnassos()
+	{
+		super("Halikarnassos", "loom");
 	}
-
-	public String getResWonder(int stage) {
-		if (stage == 1)
+	
+	public String getResWonder(int stage)
+	{
+		if(stage == 1)
 			return "clay clay";
-		else if (stage == 2)
+		else if(stage == 2)
 			return "ore ore ore";
 		else
 			return "cloth cloth";
 	}
-
-	public String runPhase2() {
-		return "at the end of the round look through discard pile and build a card for free";
-	}
+	
 }
