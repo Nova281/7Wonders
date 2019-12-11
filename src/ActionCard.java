a46070919c89a1646c0dc5bf1e9bec41fab0d6a4
AlexLee8210 <39893161+AlexLee8210@users.noreply.github.com>
2019-12-10 23:33:05 -0600

    a

--- src/ActionCard.java
+++ src/ActionCard.java
@@ -26,49 +26,10 @@ public class ActionCard extends Card{
 		}
 		else if(getAge() == 2)
 		{
-			if(getName().equals("Vineyard"))
-				effect = "Brown 1";
-			else if(getName().equals("Forum"))
-			{
+			if(getName().equals("Forum"))
 				effect = "loom glass papyrus";
-			}
 			else if(getName().equals("Caravansery"))
 				effect = "clay stone ore wood";
-		}
-		else if(getAge() == 3 && getColor().equals("gold"))
-		{
-			if(getName().equals("Haven"))
-				effect = "1 brown";
-			else if(getName().equals("Lighthouse"))
-				effect = "1 gold";
-			else if(getName().equals("Arena"))
-				effect = "3 1 wonder";
-		}
-		else
-		{
-			if(getColor().contentEquals("purple"))
-			{
-				if(getName().equals("Workers Guild"))
-					effect = "1 Brown";
-				else if(getName().equals("Craftsmens Guild"))
-					effect = "1 Silver";
-				else if(getName().equals("Traders Guild"))
-					effect = "1 Gold";
-				else if(getName().equals("Philosophers Guild"))
-					effect = "1 Green";
-				else if(getName().equals("Spies Guild"))
-					effect = "1 Red";
-				else if(getName().equals("Strategists Guild"))
-					effect = "1 Loss";
-				else if(getName().equals("Shipowners Guild"))
-					effect = "1 Brown Purple Silver";
-				else if(getName().equals("Scientists Guild"))
-					effect = "1 Science";
-				else if(getName().equals("Magistrates Guild"))
-					effect = "1 Blue";
-				else if(getName().equals("Builders Guild"))
-					effect = "1 Wonder";
-			}
 		}
 	}
 	public String getEffect()
